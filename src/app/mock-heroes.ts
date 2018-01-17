302fece5ab1e2590a6a98e970a859169becad7f4
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-01-17 18:31:33 +0200

    first commit: hero and hero detail components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2,  name: 'Antman' },
+  { id: 3,  name: 'Spidey' },
+  { id: 4,  name: 'Green Lantern' },
+  { id: 5,  name: 'Capn Crunch' },
+  { id: 6,  name: 'Black Widow' },
+  { id: 7,  name: 'Hulk' },
+  { id: 8,  name: 'Dr Strange' },
+  { id: 9,  name: 'Thor' },
+  { id: 10, name: 'Zeus' },
+  { id: 11, name: 'Iron Man' }
+];
